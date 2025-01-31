04eaf3ac19cbf160fc0e2c32e24e604824d9af96
benjaminsennes <bsennes.ieu2023@student.ie.edu>
2025-01-31 11:24:03 +0100

    added simple string

---
+++ simple_string.py
@@ -0,0 +1,18 @@
+s = "Hello"
+print(s)
+s2 = "Hello Sir Ben"
+print(s2)
+
+s3 = 'And she said : "hello"'
+print(s3)
+
+print("\"\"\"\"")
+
+s = "Hello!"
+print(s[2], s[4]) # you index by position, first index is 0
+print(s[-1]) # go around to the end!
+print(s[1], s[-5])
+
+
+
+
